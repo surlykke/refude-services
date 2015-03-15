2bc1d4727b5b809cefe04cc5eceeb4ba5e8f0f19
Christian Surlykke <christian@surlykke.dk>
2015-03-15 21:05:31 +0100

    Minor cleanup

--- service/api/service.cpp
+++ service/api/service.cpp
@@ -27,20 +27,22 @@ ServiceListener::~ServiceListener()
 
 void ServiceListener::setup(const char* socketPath)
 {
-	if (strlen(socketPath) >= UNIX_PATH_MAX)
+	if (strlen(socketPath) >= UNIX_PATH_MAX - 1)
 	{
 		error(1, errno, "socketPath to long");
 	}
-		
-	struct sockaddr_un sockaddr;
-	sockaddr.sun_family = AF_UNIX; strcpy(sockaddr.sun_path, socketPath); 
 	
+	struct sockaddr_un sockaddr;
+	memset(&sockaddr, 0, sizeof(struct sockaddr_un));
+	sockaddr.sun_family = AF_UNIX; 
+	strncpy(&sockaddr.sun_path[0], socketPath, strlen(socketPath)); 
+
 	if ((listenSocket = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)	
 	{
 		error(1, errno, "creation of listen socket failed");
 	}
 
-	if ((bind(listenSocket, (struct sockaddr*)(&sockaddr), sizeof(struct sockaddr_un))) < 0)
+	if (bind(listenSocket, (struct sockaddr*)(&sockaddr), sizeof(sa_family_t) + strlen(socketPath) + 1) < 0)
 	{
 		error(1, errno, "binding to listen socket failed");
 	}
