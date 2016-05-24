bf23d1d105ddfd4c76b58d13c9f53f9e1d2db4e6
Christian Surlykke <christian@surlykke.dk>
2016-05-24 07:38:04 +0200

    Do double fork when launching applicatons to avoid zoombies

--- desktop-service/runapplication.cpp
+++ desktop-service/runapplication.cpp
@@ -16,51 +16,56 @@ namespace org_restfulipc
     {
         pid_t pid = fork();
         if ( pid == 0) {
-            setsid(); // Detach
-            // In child process. Soon to exit, so we don't care about memory leaks
-            char* buf = strdup(appAndArguments);
+            pid_t second_pid = fork();
+            if (second_pid == 0) {
+                setsid(); // Detach
+                // In child process. Soon to exit, so we don't care about memory leaks
+                char* buf = strdup(appAndArguments);
 
-            // Remove '%u', '%U', '%f' and '%F'
-            for (char* c = buf; *c; ) {
-                if (*(c++) == '%' && (*c == 'u' || *c == 'U' || *c == 'f' || *c == 'F')) {
-                    *(c - 1) = *c = ' ';
+                // Remove '%u', '%U', '%f' and '%F'
+                for (char* c = buf; *c; ) {
+                    if (*(c++) == '%' && (*c == 'u' || *c == 'U' || *c == 'f' || *c == 'F')) {
+                        *(c - 1) = *c = ' ';
+                    }
                 }
-            }
 
-            char* argv[32]; 
-            int argc = 0;
+                char* argv[32]; 
+                int argc = 0;
 
-            bool justSawWhitespace = true;
-            for (char* c = buf; *c; c++) {
-                if (isspace(*c)) {
-                    *c = '\0';
-                    justSawWhitespace = true;
-                }
-                else {
-                    if (justSawWhitespace) {
-                        argv[argc++] = c;
-                        justSawWhitespace = false;
-                        if (argc >= 31) {
-                            throw RuntimeError("Too many arguments: %s\n", appAndArguments);
+                bool justSawWhitespace = true;
+                for (char* c = buf; *c; c++) {
+                    if (isspace(*c)) {
+                        *c = '\0';
+                        justSawWhitespace = true;
+                    }
+                    else {
+                        if (justSawWhitespace) {
+                            argv[argc++] = c;
+                            justSawWhitespace = false;
+                            if (argc >= 31) {
+                                throw RuntimeError("Too many arguments: %s\n", appAndArguments);
+                            }
                         }
                     }
                 }
+
+                argv[argc] = NULL;
+
+                std::cout << "Doing: ";
+                for (char** ptr = argv; *ptr; ptr++) {
+                    std::cout << *ptr << " ";
+                }
+                std::cout << "\n";
+
+                execvp(argv[0], argv);
+                
+                // If we arrive here an error occurred;
+                int errorNumber = (errno);
+                dprintf(2, "Error: doing execvpe: %s\n", errno);
             }
-
-            argv[argc] = NULL;
-
-            std::cout << "Doing: ";
-            for (char** ptr = argv; *ptr; ptr++) {
-                std::cout << *ptr << " ";
+            else {
+                exit(0);
             }
-            std::cout << "\n";
-
-            execvp(argv[0], argv);
-            
-            // If we arrive here an error occurred;
-            int errorNumber = (errno);
-            dprintf(2, "Error: doing execvpe: %s\n", errno);
-            exit(errorNumber);
         }
         else if (pid < 0) {
             // In parent process, child creation failed
