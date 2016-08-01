b1d7ba7c0a14f78918a3c139574bda96c1fb60f2
Christian Surlykke <christian@surlykke.dk>
2016-08-01 10:08:32 +0200

    Ditch nginx in favour of haproxy

--- nginx/runNginx.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-NGINXDIR=$XDG_RUNTIME_DIR/nginx
-mkdir -p $NGINXDIR
-sed 's@NGINXDIR@'"$XDG_RUNTIME_DIR/nginx"'@' nginx.conf.template > $NGINXDIR/nginx.conf
-nginx -c $NGINXDIR/nginx.conf 
-
